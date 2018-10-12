a835a8cff46479d459576bb92438ed8145c1cc60
807119680 <807119680@qq.com>
2018-10-12 20:29:08 +0800

    Update swift9.29-3/main.swift
    一个排序输出问题

--- swift9.29-3/main.swift
+++ swift9.29-3/main.swift
@@ -7,6 +7,20 @@
 //
 
 import Foundation
-
-print("Hello, World!")
-
+var a:[Int]=[15,23,5,8,12,36,100,56,9,10]
+for i in 0..<a.count
+{
+    for j in 0..<a.count - (i+1)
+    {
+        if a[j]>a[j+1]
+            {
+              let temp = a[j]
+              a[j] = a[j+1]
+              a[j+1] = temp
+             }
+    }
+}
+for i in 0..<a.count
+{
+    print(a[i])
+}
